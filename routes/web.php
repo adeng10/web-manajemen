772d7aa00c070b5ab9f5656f456eeec3fe335606
adeng10 <aidilkamal05@gmail.com>
2025-05-01 17:19:09 +0700

    part3

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-Route::get('dashboard', [DashboardController::class, 'index']);
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
